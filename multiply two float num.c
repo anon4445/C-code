44b186fa1531ecdb2b299d5174d5c181e9bb8143
anon4445 <tahrim.anon21@gmail.com>
2022-04-20 13:15:53 -0700

    6th

---
+++ multiply two float num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a, b, product;
+    scanf("%f %f",&a,&b);
+    product = a*b;
+
+    printf("product of the num :%f",product);
+
+    return 0;
+}
+
+
